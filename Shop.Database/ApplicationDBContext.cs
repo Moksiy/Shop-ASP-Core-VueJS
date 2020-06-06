80cf9f5fcef704e6d777d8b0ac35f0e2dcd5da76
Moksiy <maksaligin@gmail.com>
2020-06-06 15:31:06 +0300

    Добавьте файлы проекта.

---
+++ Shop.Database/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Database
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext()
+        {
+
+        }
+    }
+}
