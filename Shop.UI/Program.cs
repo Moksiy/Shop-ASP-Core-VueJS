0fb8da16335a6e10ad1d1b4c2f4f1778de27e8a3
Moksiy <maksaligin@gmail.com>
2020-06-18 17:41:07 +0300

    Claims authorization

--- Shop.UI/Program.cs
+++ Shop.UI/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Shop.Database;
 
 namespace Shop.UI
 {
@@ -13,7 +17,47 @@ namespace Shop.UI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            try
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+
+                    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                    context.Database.EnsureCreated();
+
+                    if(!context.Users.Any())
+                    {
+                        var adminUser = new IdentityUser() 
+                        {
+                            UserName = "Admin"
+                        };
+
+                        var managerUser = new IdentityUser()
+                        {
+                            UserName = "Manager"
+                        };
+
+                        userManager.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
+                        userManager.CreateAsync(managerUser, "password").GetAwaiter().GetResult();
+
+                        var adminClaim = new Claim("Role", "Admin");
+                        var managerClaim = new Claim("Role", "Manager");
+
+                        userManager.AddClaimAsync(adminUser, adminClaim).GetAwaiter().GetResult();
+                        userManager.AddClaimAsync(managerUser, managerClaim).GetAwaiter().GetResult();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Shop.UI/Startup.cs
+++ Shop.UI/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,32 @@ namespace Shop.UI
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(Configuration["DefaultConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => 
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<ApplicationDBContext>();
+
+            services.ConfigureApplicationCookie(options => 
+            {
+                options.LoginPath = "/Accounts/Login";
+            });
+
+            services.AddAuthorization(options =>             
+            {
+                options.AddPolicy("Admin", policy => policy.RequireClaim("Role", "Admin"));
+                options.AddPolicy("Manager", policy => policy.RequireClaim("Role", "Manager"));
+            });
+
+            services.AddMvc().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AuthorizeFolder("/Admin");
+            }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
             services.AddDistributedMemoryCache();
 
             services.AddSession(options =>
@@ -60,14 +87,16 @@ namespace Shop.UI
             app.UseAuthentication();
             app.UseAuthorization();
             app.UseSession();
+            app.UseAuthentication();
+            
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapDefaultControllerRoute();
-                endpoints.MapControllers(); //
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers(); //+
                 endpoints.MapRazorPages();
             });
 
-            //app.UseMvc();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
